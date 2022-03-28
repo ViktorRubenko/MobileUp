bf1bb6f5d0e3fdf24f59cba502502a3028f946c9
Victor Rubenko <vrubenko@gmail.com>
2022-03-28 18:05:37 +0300

    Add AuthManager.

---
+++ MobileUp/Managers/AuthManager.swift
@@ -0,0 +1,90 @@
+//
+//  AuthManager.swift
+//  MobileUp
+//
+//  Created by Victor Rubenko on 28.03.2022.
+//
+
+import Foundation
+
+enum AuthError: Error {
+    case authError
+}
+
+final class AuthManager {
+    
+    private enum Constants {
+        static let clientID = "8118560"
+        static let redirectURI = "https://oauth.vk.com/blank.html"
+        static let responseType = "token"
+        static let display = "mobile"
+        static let authURL = "https://oauth.vk.com/authorize"
+    }
+    
+    private enum Keys: String {
+        case accessToken, refreshToken, expirationDate
+    }
+    
+    static let shared = AuthManager()
+    
+    private init() {}
+    
+    private var accessToken: String? {
+        UserDefaults.standard.string(forKey: Keys.accessToken.rawValue)
+    }
+    
+    private var expirationDate: Date? {
+        UserDefaults.standard.value(forKey: Keys.expirationDate.rawValue) as? Date
+    }
+    
+    private var shouldRefreshToken: Bool {
+        guard let expirationDate = expirationDate else {
+            return false
+        }
+        return Date().addingTimeInterval(300) >= expirationDate
+    }
+    
+    public var isSignIn: Bool {
+        accessToken != nil
+    }
+    public var signInURL: URL? {
+        var components = URLComponents(string: Constants.authURL)
+        
+        components?.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.clientID),
+            URLQueryItem(name: "display", value: Constants.display),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: Constants.responseType),
+        ]
+        
+        return components?.url
+    }
+    
+    public func getAccessToken(_ urlString: String) -> Result<Bool, AuthError> {
+        if !urlString.starts(with: Constants.redirectURI) {
+            return .success(false)
+        }
+        let sanitizedURL = urlString.replacingOccurrences(of: "#", with: "?")
+        let items = URLComponents(string: sanitizedURL)?.queryItems
+        guard let accessToken = items?.first(where: { $0.name == "access_token" })?.value else {
+            return .failure(.authError)
+        }
+        guard let expiresIn = items?.first(where: { $0.name == "expires_in" })?.value,
+                let intExpiresIn = Int(expiresIn) else {
+            return .failure(.authError)
+        }
+        
+        AuthManager.shared.setAcessToken(accessToken: accessToken, expiresIn: intExpiresIn)
+        return .success(true)
+    }
+    
+    private func setAcessToken(accessToken: String, expiresIn: Int) {
+        UserDefaults.standard.set(accessToken, forKey: Keys.accessToken.rawValue)
+        UserDefaults.standard.set(Date().addingTimeInterval(TimeInterval(expiresIn)), forKey: Keys.expirationDate.rawValue)
+    }
+    
+    public func sightOut() {
+        UserDefaults.standard.removeObject(forKey: Keys.accessToken.rawValue)
+        UserDefaults.standard.removeObject(forKey: Keys.expirationDate.rawValue)
+    }
+}
